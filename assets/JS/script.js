00e5fa947f377ddb3d0e4a4497bb3d7c9988421c
Luizinho101 <luiz.antonio.da.silva777@gmail.com>
2026-03-18 23:12:16 -0300

    refactore: removendo codigo de teste

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -4,9 +4,7 @@ function capturaDados(){
     let dataInicioTarefa = document.getElementById('dataInicio').value;
     let dataFimTarefa = document.getElementById('dataFim').value;
 
-    document.getElementById("resultado").innerHTML = "Você digitou: " + tarefa;
-    document.getElementById("resultado2").innerHTML = "Você digitou: " + dataInicioTarefa;
-    document.getElementById("resultado3").innerHTML = "Você digitou: " + dataFimTarefa;
+    
 
     
 }
